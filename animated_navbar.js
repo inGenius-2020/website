2cd41e649f124582d4553186d190a17158d2fde2
Amitha Nayak <nayak.amit.blr@gmail.com>
2020-09-07 02:13:35 +0530

    animated navbar

---
+++ animated_navbar.js
@@ -0,0 +1,22 @@
+  $('.navTrigger').click(function () {
+      $(this).toggleClass('active');
+      //console.log("Clicked menu");
+      $("#mainListDiv").toggleClass("show_list");
+      $("#mainListDiv").fadeIn();
+
+  });
+  $('.nav-item').click(function () {
+      //console.log("Clicked menu");
+      $("#mainListDiv").toggleClass("show_list");
+      $("#mainListDiv").fadeIn();
+      $('.navTrigger').toggleClass('active');
+
+  });
+  $(window).scroll(function() {
+          if ($(document).scrollTop() > 50) {
+              $('.nav').addClass('affix');
+              //console.log("OK");
+          } else {
+              $('.nav').removeClass('affix');
+          }
+  });
