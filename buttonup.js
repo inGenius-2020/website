be9036cd5ac97be97af9e25b01f58ba0651ae2fe
Amitha Nayak <nayak.amit.blr@gmail.com>
2020-09-08 13:10:42 +0530

    smooth-scroll

--- buttonup.js
+++ buttonup.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-
 	// Validación para cuando le de click al botón de ir arriba y al botón del footer-mobil lo lleve a la posicion 0 del scroll
 	$('#btn-go-to-up').on('click', function() {
 		$('html').animate({
 			scrollTop: '0px'
-		}, 1000);
+		}, 100);
 	});
 
 	$(window).on('load scroll resize', function() {
+		if($(this).scrollTop() > $(window).height()-10)
+			$('#btn-go-to-up').css('display','block');
 		if ($(this).scrollTop() > $(window).height()) {
 			// Si el scroll pasa de "winH" el botón de ir arriba aparece
+			$('#btn-go-to-up').css('display','block');
 			$('#btn-go-to-up').removeClass('hide-btn-go-to-up');
 		}
 
@@ -21,6 +23,7 @@ $(document).ready(function() {
 
 	// Validación para cunado le dan click a los botones al que le de click no se quede con el focus marcado
 	$('button').on('click', function() {
+		//console.log('click');
 		$(this).blur();
 	});
 
