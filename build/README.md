11e186598ce85d11b9944e83eaeb5900de3730f4
Akshat <ak127a@gmail.com>
2020-09-16 00:16:34 +0530

    build folder added

---
+++ build/README.md
@@ -0,0 +1,4 @@
+# website
+The main website for inGenius 2020
+
+run with npm start
---
+++ build/js/animated_navbar.js
@@ -0,0 +1,30 @@
+ $('.navTrigger').click(function () {
+      $(this).toggleClass('active');
+      //console.log("Clicked menu");
+      $("#mainListDiv").toggleClass("show_list");
+      $("#mainListDiv").fadeIn();
+
+  });
+  $('.nav-item').click(function () {
+      //console.log("Clicked menu");
+      $("#mainListDiv").toggleClass("show_list");
+      $("#mainListDiv").fadeIn();
+      $('.navTrigger').toggleClass('active');
+
+  });
+  $(document).scroll(function() {
+      if($(document).scrollTop()>15){
+        $('.anim_addi').removeClass('d-none');
+        console.log("here");
+      }
+      else{
+          $('.anim_addi').addClass('d-none');
+      }
+
+      if ($(document).scrollTop() > 50) {
+          $('.nav').addClass('affix');
+              //console.log("OK");
+      } else {
+          $('.nav').removeClass('affix');
+          }
+  });
