4eeb75905f71eaed5770f4af87e19cd9e8ac9088
Abhay Aggrawal <abhayagarwal383@gmail.com>
2020-09-18 11:56:42 +0530

    date fix

--- js/countdown.js
+++ js/countdown.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date("October 31, 2020 00:00:00");
 initializeClock('clockdiv', deadline);
